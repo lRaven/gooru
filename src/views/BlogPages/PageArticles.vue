32e6698ae16cd7df9b0abe93b7b2dafd25d5ef20
Dmitriy Gorbatenko <gorbatenko-2000@bk.ru>
2022-09-20 17:15:21 +0300

    feat: добавить обработку отображения для пустого раздела блога

--- src/views/BlogPages/PageArticles.vue
+++ src/views/BlogPages/PageArticles.vue
@@ -1,10 +1,17 @@
 <template>
 	<section class="page-articles">
-		<article-card
-			v-for="article in articles"
-			:article="article"
-			:key="article.id"
-		/>
+		<template v-if="articles.length">
+			<article-card
+				v-for="article in articles"
+				:article="article"
+				:key="article.id"
+			/>
+		</template>
+		<div v-else class="empty">
+			<span  class="empty__text">Здесь пока пусто</span>
+			<img class="empty__image" src="/img/blog/emptyBlog.svg" alt="картинка пустых папок" />
+		</div>
+		
 	</section>
 </template>
 
@@ -23,23 +30,41 @@
 			...mapGetters(["getArticlesByTopic"]),
 			articles() {
 				const currentParam = this.$route.params.id;
-				if (currentParam === 'all') {
+				if (currentParam === "all") {
 					return store.state.blog.articles;
 				}
 				return this.getArticlesByTopic(+currentParam);
 			},
-		}
+		},
 	};
 </script>
 
 <style lang="scss" scoped>
+	@import "@/assets/scss/variables";
 	.page-articles {
-    display: grid;
-    grid-gap: 5rem 0;
-    padding: 0;
+		display: grid;
+		grid-gap: 5rem 0;
+		padding: 0;
 		justify-content: center;
 		@media (max-width: 600px) {
 			padding: 0 1rem;
+		}
+	}
+	.empty {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 5rem 0 0 0;
+		&__text {
+			font-size: 2.4rem;
+			font-weight: 500;
+			margin: 0 0 5rem 0;
+			color: $dark
+		}
+		&__image {
+			width: 100%;
+			object-fit: contain;
+			object-position: center;
 		}
 	}
 </style>
