e26ea4f5ca6e7ed00e8a1cdabbc7da60f10abef8
Dmitriy Gorbatenko <gorbatenko-2000@bk.ru>
2022-10-05 10:35:59 +0300

    fix: скрыть блок рефералки при ожидании подтверждения платежа по тарифу

--- src/views/CabinetPages/PageRates.vue
+++ src/views/CabinetPages/PageRates.vue
@@ -143,7 +143,7 @@
 				</div>
 			</template>
 			<div
-				v-if="hasSelectedTariff && documentWidth > 700"
+				v-if="!pendingForCapture && documentWidth > 700"
 				class="referal page-rates__referal"
 			>
 				<h3 class="referal__title">Реферальная программа</h3>
